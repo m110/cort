dd9e29865a72380af57685231012e97c999c5d8b
m110 <m110@m110.pl>
2015-02-26 19:43:25 +0100

    Add basic Service implementation.

---
+++ service/service.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"github.com/m110/cort/rpc"
+)
+
+type Service struct {
+	name   string
+	server *rpc.Server
+}
+
+func NewService(name string, uri string) *Service {
+	service := &Service{
+		name:   name,
+		server: rpc.NewServer(uri),
+	}
+
+	return service
+}
+
+func (s *Service) Start() error {
+	var err error
+
+	err = s.server.Start()
+	if err != nil {
+		return err
+	}
+
+	err = s.Register()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) Stop() error {
+	var err error
+
+	err = s.Deregister()
+	if err != nil {
+		return err
+	}
+
+	err = s.server.Stop()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) Register() error {
+	// TODO Register the service to be discovered by other services
+	return nil
+}
+
+func (s *Service) Deregister() error {
+	// TODO Deregister the service
+	return nil
+}
