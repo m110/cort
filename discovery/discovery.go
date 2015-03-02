c82642bc7f615d6675e82caed91b2d01c4b1f7c1
m110 <m110@m110.pl>
2015-03-02 22:56:28 +0100

    Discovery: add basic message routing.

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -3,6 +3,8 @@ package discovery
 import (
 	zmq "github.com/pebbe/zmq4"
 	"log"
+
+	"github.com/m110/cort/resources/consul"
 )
 
 type Discovery struct {
@@ -11,6 +13,8 @@ type Discovery struct {
 
 	remoteSocket *zmq.Socket
 	localSocket  *zmq.Socket
+
+	nextNode chan Node
 }
 
 var discovering = map[string]*Discovery{}
@@ -56,6 +60,16 @@ func (d *Discovery) Start() error {
 		return err
 	}
 
+	d.nextNode = make(chan Node)
+
+	nodesManager, err := consul.NewConsulProxy()
+	if err != nil {
+		return err
+	}
+
+	watcher := NewWatcher(d.service, d.nextNode, nodesManager)
+	watcher.Start()
+
 	d.running = true
 	go d.serve()
 
@@ -70,15 +84,64 @@ func (d *Discovery) serve() {
 	for d.running {
 		polled, err := poller.Poll(100)
 		if err != nil {
-			// TODO Log a warning
+			log.Println("ZMQ poll failed:", err)
+			continue
 		}
 
 		if len(polled) > 0 {
-			// TODO Handle messages
+			for _, p := range polled {
+				switch socket := p.Socket; socket {
+				case d.remoteSocket:
+					err := d.handleRemoteSocket()
+					if err != nil {
+						log.Println("Remote socket error:", err)
+					}
+				case d.localSocket:
+					err := d.handleLocalSocket()
+					if err != nil {
+						log.Println("Local socket error:", err)
+					}
+				}
+			}
 		}
 	}
 
 	d.cleanUp()
+}
+
+func (d *Discovery) handleRemoteSocket() error {
+	msg, err := d.remoteSocket.RecvMessage(0)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.localSocket.SendMessage(msg)
+
+	return err
+}
+
+func (d *Discovery) handleLocalSocket() error {
+	msg, err := d.localSocket.RecvMessage(0)
+	if err != nil {
+		return err
+	}
+
+	node := <-d.nextNode
+	if node.Err != nil {
+		d.sendError(d.localSocket, msg, node.Err)
+		return node.Err
+	}
+
+	log.Printf("Routing message to %s (%s): %s\n", d.service, node.Uri, msg[1:])
+	_, err = d.remoteSocket.SendMessage(msg)
+
+	return err
+}
+
+func (d *Discovery) sendError(socket *zmq.Socket, msg []string, err error) error {
+	errMsg := append(msg[0:len(msg)-1], err.Error())
+	_, err = socket.SendMessage(errMsg)
+	return err
 }
 
 func (d *Discovery) cleanUp() {
